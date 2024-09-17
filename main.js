34b168b1235df6c17240e6e2655a7fe91da40852
hritikcore <hritiksethi@Hritiks-MacBook-Pro.local>
2024-09-17 13:56:54 +0530

    checking redirection

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // Function to detect if the user is on mobile and redirect to the mobile site
-function redirectToMobile() {
-  const isMobile = window.innerWidth <= 768; // Define mobile width threshold
-  const currentUrl = window.location.href;
+// function redirectToMobile() {
+//   const isMobile = window.innerWidth <= 768; // Define mobile width threshold
+//   const currentUrl = window.location.href;
 
-  // Check if the current URL is the desktop site on custom domain and user is on a mobile device
-  if (isMobile && currentUrl.includes("maaher.life")) {
-    window.location.href = "https://maahermobile.vercel.app";
-  }
+//   // Check if the current URL is the desktop site on custom domain and user is on a mobile device
+//   if (isMobile && currentUrl.includes("maaher.life")) {
+//     window.location.href = "https://maahermobile.vercel.app";
+//   }
 
-  // Optional: Redirect back to desktop if user resizes window to desktop size
-  if (!isMobile && currentUrl.includes("maaher.life")) {
-    window.location.href = "https://maaher.vercel.app";
-  }
-}
+//   // Optional: Redirect back to desktop if user resizes window to desktop size
+//   if (!isMobile && currentUrl.includes("maaher.life")) {
+//     window.location.href = "https://maaher.vercel.app";
+//   }
+// }
 
 // Call the function on page load
 window.onload = redirectToMobile;
